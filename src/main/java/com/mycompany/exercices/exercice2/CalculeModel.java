41e1f5612b80088f8aa6da89a3394a667d44bb1e
khalil <khalillakhdharatc@gmail.com>
2022-02-20 11:40:26 +0100

    ex2

---
+++ src/main/java/com/mycompany/exercices/exercice2/CalculeModel.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exercices.exercice2;
+
+/**
+ *
+ * @author khali
+ */
+public class CalculeModel {
+    private int intier1,entier2;
+
+    public int getIntier1() {
+        return intier1;
+    }
+
+    public void setIntier1(int intier1) {
+        this.intier1 = intier1;
+    }
+
+    public int getEntier2() {
+        return entier2;
+    }
+
+    public void setEntier2(int entier2) {
+        this.entier2 = entier2;
+    }
+
+    public CalculeModel() {
+    }
+
+    public CalculeModel(int intier1, int entier2) {
+        this.intier1 = intier1;
+        this.entier2 = entier2;
+    }
+    
+}
