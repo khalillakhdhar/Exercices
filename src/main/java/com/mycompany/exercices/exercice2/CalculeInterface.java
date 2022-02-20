eb9f39627d0fe5f5762f6cf696f0c89d9bc748ae
khalil <khalillakhdharatc@gmail.com>
2022-02-20 11:43:09 +0100

    interface

---
+++ src/main/java/com/mycompany/exercices/exercice2/CalculeInterface.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.exercices.exercice2;
+
+/**
+ *
+ * @author khali
+ */
+public interface CalculeInterface {
+    int somme(CalculeModel c);
+    int produit(CalculeModel c);
+    int soustraction(CalculeModel c);
+    int division(CalculeModel c);
+    int factoriel(CalculeModel c);
+    int Pgcd(CalculeModel c);
+    int Ppcm(CalculeModel c);
+}
